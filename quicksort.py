a2453efb8635a9904c618924d2f83ec62b2d23e6
Eric <ewust@umich.edu>
2012-12-07 04:52:05 -0500

    freq should take values not index (useful for quicksort and mergesort)

---
+++ quicksort.py
@@ -0,0 +1,55 @@
+
+
+from sort import Sort
+from bulbs import Bulbs
+from remote import RemoteDriver
+import time
+
+class QuickSort(Sort):
+    def __init__(self, bulbs):
+        super(QuickSort, self).__init__(bulbs)
+
+    # From Wikipedia's pseudo code of an in-place quicksort
+    def partition(self, left, right, pivot_idx): 
+        pivot_value = self.lights[pivot_idx]
+        self.swap(pivot_idx, right)
+        if pivot_idx != right:
+            self.render()
+            time.sleep(0.05)
+        tmp_idx = left
+        for i in range(left, right):
+            if self.freq(self.lights[i]) < self.freq(pivot_value):
+                self.swap(i, tmp_idx)
+                tmp_idx += 1
+                if i != tmp_idx:
+                    self.render()
+                    time.sleep(0.05)
+        self.swap(tmp_idx, right)
+        if tmp_idx != right:
+            self.render()
+            time.sleep(0.05)
+        return tmp_idx 
+    
+    def quicksort(self, left, right):
+        if left < right:
+            pivot_idx = (left + right)/2     # play with different pivot selection
+            pivot_new_idx = self.partition(left, right, pivot_idx)
+            self.quicksort(left, pivot_new_idx - 1)
+            self.quicksort(pivot_new_idx + 1, right)
+
+    def sort(self):
+        self.quicksort(0, 99)
+        
+
+if __name__=="__main__":
+    print 'waiting...'
+    d = RemoteDriver("QuickSort")
+    print 'our turn'
+    
+    sorter = QuickSort(Bulbs(d))
+    sorter.sort()
+    
+    # Leave the sorted list up
+    d.busy_wait()
+    
+    
