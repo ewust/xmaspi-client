fac254918c159a76c3e94e56ba4305bb1b047d9f
Pat Pannuto <pat.pannuto@gmail.com>
2012-12-07 06:05:45 -0500

    single ball working

--- physics.py
+++ physics.py
@@ -7,6 +7,8 @@ from remote import RemoteDriver
 
 import random
 
+NUM_BALLS = 1
+
 print "waiting our turn..."
 if len(sys.argv) > 1:
 	d = RemoteDriver(name="test", addr="localhost")
@@ -17,18 +19,22 @@ bulbs = Bulbs(d)
 
 
 # Start by drawing bumpers at the edge
+bumpers = set()
 for f in range(5):
 	bulbs.frame[f] = Bulbs.BLUE
+	bumpers.add(f)
 for f in range(95,100):
 	bulbs.frame[f] = Bulbs.BLUE
+	bumpers.add(f)
+field = set(range(100)) - bumpers
 
 # Now add the balls
-balls = random.sample(range(10,90), 4)
+balls = random.sample(range(10,90,2), NUM_BALLS)
 for b in balls:
 	if random.randint(0,1):
-		bulbs.frame[b] = (15,0,0,random.randint(0,255))
+		bulbs.frame[b] = (15,0,0,random.randint(150,255))
 	else:
-		bulbs.frame[b] = (0,15,0,random.randint(0,255))
+		bulbs.frame[b] = (0,15,0,random.randint(150,255))
 
 # Draw everything
 bulbs.render(force=True)
@@ -52,16 +58,15 @@ for blinks in range(3):
 def ghost(ball):
 	a = bulbs.frame[ball][3]
 	if bulbs.frame[ball][0]:
-		# Red is down
-		d = -1
-	else:
 		d = 1
+	else:
+		d = -1
 
 	a = a >> 1
 	while (a > 0):
 		r,g,b,ign = bulbs.frame[ball]
 
-		ball += d
+		ball -= d
 		if sum(bulbs.frame[ball][2:]):
 			return
 		a = a >> 1
@@ -71,5 +76,51 @@ for b in balls:
 	ghost(b)
 
 bulbs.render()
+
+def advance(ball):
+	r,g,b,a = bulbs.frame[ball]
+	bulbs.frame[ball] = (0,0,0,0) # 0 for now, will be ghosted later
+
+	if r:
+		d = 1
+	elif g:
+		d = -1
+	else:
+		print "Something has gone wrong"
+		print "Current ball:", ball
+		print (r,g,b,a)
+		print
+		print bulbs.frame
+
+	if ball+d in bumpers:
+		d = -d
+		ball = ball + d
+		bulbs.frame[ball] = (g,r,b,a>>1)
+		print "Hit bumper. Ball %d is now (%d %d %d %d)" % (ball, g,r,b,a>>1)
+	elif ball+d in balls:
+		print "Collided with ", ball+d
+		other_idx = ball+d
+		other = bulbs.frame[other_idx]
+
+		d = -d
+		ball = ball + d
+		bulbs.frame[ball] = other
+
+		bulbs.frame[other_idx] = (r,g,b,a)
+	else:
+		ball = ball + d
+		bulbs.frame[ball] = (r,g,b,a)
+
+	return ball
+
+while True:
+	for i in range(len(balls)):
+		balls[i] = advance(balls[i])
+	for b in balls:
+		ghost(b)
+	bulbs.render()
+	for b in field - set(balls):
+		bulbs.frame[b] = (0,0,0,0)
+	d.busy_wait(.1)
 
 d.busy_wait(5)
