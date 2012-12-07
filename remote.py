61b37ed8cdf24b7bde76b95fb7b2367b2afe0e28
Pat Pannuto <pat.pannuto@gmail.com>
2012-12-07 02:26:12 -0500

    flesh out more of demo app

--- remote.py
+++ remote.py
@@ -59,12 +59,22 @@ if __name__=="__main__":
     # Turn off all the LEDs 
     for i in range(100):
         d.write_led(i, 0, 0, 0, 0)
-        
 
-    # Turn them back on from the top, with some delay
+    # Turn them back on from the top, with some delay (100*.05 = 5s)
     for i in range(100):
         d.write_led(i, 200, 13, 0, 13)
         time.sleep(.05)
+
+    # Turn them off in chunks of 10 (10*2 = 20s)
+    # Note that busy_wait must be used for delays >= 1s
+    for i in range(100, 0, -10):
+        for j in range(i, i-10, -1):
+            d.write_led(j-1, 0, 0, 0, 0)
+        d.busy_wait(2)
+
+    # Turn on all the LEDs 
+    for i in range(100):
+        d.write_led(i, 200, 13, 13, 13)
     
     # Send NOP keep-alives until our 30-second time expires
     # Alternatively, you can close the connection with
